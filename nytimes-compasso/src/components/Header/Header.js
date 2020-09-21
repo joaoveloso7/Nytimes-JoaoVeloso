47ef2fe7a366ebfb3fe691df67865ad0f54e20a7
joaoveloso7 <59774054+joaoveloso7@users.noreply.github.com>
2020-09-21 00:07:21 -0300

    feat(Header): Adicionando variaveis de tempo para mostrar na tela os dias da semana, meses, dias e ano atuais.

--- nytimes-compasso/src/components/Header/Header.js
+++ nytimes-compasso/src/components/Header/Header.js
@@ -4,13 +4,25 @@ import './Header.css'
 import logo from '../../assets/logonytimes.svg'
 
 export default function Header() {
+
+
+    //dia, mes numero e ano
+
+    const time = new Date();
+    let dias = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+    let mes = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December')
+    let dia = time.getDate();
+    let ano = time.getFullYear();
+
+
+
     return (
         <header>
             <div className="logo-container">
                 <img id="logo" src={logo} alt="logo" />
             </div>
             <div className="date-time">
-                <p>Tuesday, September 22. 2020</p>
+                <p>{dias[time.getDay()]},     {mes[time.getMonth()]}     {dia},   {ano}</p>
             </div>
 
 
